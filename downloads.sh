3609ad603a0e02cf95d8093628ba44e115323bbc
Finn Bear <finnbearlabs@gmail.com>
2021-10-16 22:29:37 -0700

    More robust character replacements.

--- downloads.sh
+++ downloads.sh
@@ -1,3 +1,4 @@
 wget -O test.csv https://raw.githubusercontent.com/vzhou842/profanity-check/master/profanity_check/data/clean_data.csv
 wget -O src/dictionary.txt https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt
 wget -O src/dictionary_common.txt https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english.txt
+wget -O src/unicode_confusables.txt https://www.unicode.org/Public/security/14.0.0/confusables.txt
---
+++ src/replacement_finder.rs
@@ -0,0 +1,51 @@
+use csv::Writer;
+use std::collections::BTreeMap;
+
+fn main() {
+    let replacements: BTreeMap<char, String> = include_str!("unicode_confusables.txt")
+        .split("\n")
+        .filter(|line| !line.is_empty() && !line.starts_with('#'))
+        .filter_map(|line| {
+            let mut segments = line.split(';');
+            segments
+                .next()
+                .zip(segments.next())
+                .and_then(|(find, replace)| {
+                    let find_char = u32::from_str_radix(find.trim(), 16)
+                        .ok()
+                        .and_then(char::from_u32);
+                    let replace_char = u32::from_str_radix(replace.trim(), 16)
+                        .ok()
+                        .and_then(char::from_u32)
+                        .map(|c| c.to_ascii_lowercase());
+
+                    find_char.zip(replace_char).and_then(|(find, replace)| {
+                        if replace.is_digit(36) {
+                            println!("{} -> {}", find, replace);
+                            Some((find, replace.to_string()))
+                        } else {
+                            None
+                        }
+                    })
+                })
+        })
+        .chain(
+            include_str!("replacements_extra.csv")
+                .split("\n")
+                .filter(|line| !line.is_empty())
+                .map(|line| {
+                    let comma = line.find(",").unwrap();
+                    (
+                        line[..comma].chars().next().unwrap(),
+                        String::from(&line[comma + 1..]),
+                    )
+                }),
+        )
+        .collect();
+
+    let mut writer = Writer::from_path("src/replacements.csv").unwrap();
+    for (find, replace) in replacements {
+        writer.write_record(&[&find.to_string(), &replace]).unwrap();
+    }
+    writer.flush().unwrap();
+}
